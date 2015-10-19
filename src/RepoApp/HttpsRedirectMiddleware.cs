90a72270cbe49a308c2f6f09aa1f88a5d99dafaf
Rinsen <fredrikh@outlook.com>
2015-10-19 14:45:40 +0200

    Initial commit

---
+++ src/RepoApp/HttpsRedirectMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace RepoApp
+{
+    public class HttpsRedirectMiddleware
+    {
+        readonly RequestDelegate _next;
+
+        public HttpsRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.HttpContext.Request.IsHttps)
+            {
+                HandleNonHttpsRequest(context);
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+
+        void HandleNonHttpsRequest(HttpContext context)
+        {
+            // only redirect for GET requests, otherwise the browser might not propagate the verb and request
+            // body correctly.
+            if (!string.Equals(context.Request.HttpContext.Request.Method, "GET", StringComparison.OrdinalIgnoreCase))
+            {
+                context.Response.StatusCode = 403;
+            }
+            else
+            {
+                var newUrl = string.Concat(
+                    "https://",
+                    context.Request.Host.ToUriComponent(),
+                    context.Request.PathBase.ToUriComponent(),
+                    context.Request.Path.ToUriComponent(),
+                    context.Request.QueryString.ToUriComponent());
+                
+                context.Response.Redirect(newUrl, permanent: true);
+            }
+        }
+    }
+}
---
+++ src/RepoApp/Startup.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNet.Builder;
+using Microsoft.Framework.DependencyInjection;
+
+namespace RepoApp
+{
+    public class Startup
+    {
+
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add MVC services to the services container.
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<HttpsRedirectMiddleware>();
+
+            // Add MVC to the request pipeline.
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
